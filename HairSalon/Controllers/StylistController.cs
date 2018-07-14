b3ec3217780213688c723b0a6ab292ab63e757da
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-07-14 11:04:10 -0700

    Add ReturnForm.

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -8,38 +8,54 @@ namespace HairSalon.Controllers
 {
   public class StylistController : Controller
   {
-    [HttpGet("/stylist")]
-    public ActionResult Index()
-    {
-      List<Stylist> allStylist = Stylist.GetAll();
-      return View(allStylist);
-    }
-    [HttpGet("/stylist/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
-    }
-    [HttpPost("/stylists/new")]
-    public ActionResult Create(string stylistName, string detail)
-    {
-      string newDetail = "";
-      if(!string.IsNullOrWhiteSpace(Request.Form["detail"]))
+    [HttpGet("/Stylist")]
+      public ActionResult ReturnForm()
       {
-        newDetail = detail;
+        return View();
       }
-      Stylist allStylist = new Stylist(stylistName, detail);
-      allStylist.Save();
-      return RedirectToAction("Index");
-    }
-    [HttpPost("/stylists/search")]
-    public ActionResult Search(string searchFx, string searchTerm)
-    {
-      List<Stylist> foundStylist = new List<Stylist> {};
-      if(searchFx.Equals("byStylist"))
+    [HttpPost("/Stylist/new")]
+      public ActionResult ReturnForm(string newName, string newDetail)
       {
-        foundStylist = Stylist.FindByStylistName(searchTerm);
+      Stylist newStylist = new Stylist(newName, newDetail);
+      newStylist.Save();
+        return View(newStylist);
       }
-      return View("Index", foundStylist);
-    }
+    [HttpGet("/Stylist/new")]
+      public ActionResult CreateForm()
+      {
+        return View();
+      }
+    // [HttpGet("/stylist")]
+    // public ActionResult Index()
+    // {
+    //   List<Stylist> allStylist = Stylist.GetAll();
+    //   return View(allStylist);
+    // }
+    // [HttpGet("/stylist/new")]
+    // public ActionResult CreateForm()
+    // {
+    //   return View();
+    // }
+    // [HttpPost("/stylists/new")]
+    // public ActionResult Create(string stylistName, string detail)
+    // {
+    //   string newDetail = "";
+    //   if(!string.IsNullOrWhiteSpace(Request.Form["detail"]))
+    //   {
+    //     newDetail = detail;
+    //   }
+    //   Stylist allStylist = new Stylist(stylistName, detail);
+    //   allStylist.Save();
+    //   return RedirectToAction("Index");
+    // }
+    // [HttpPost("/stylists/search")]
+    // public ActionResult Search(string searchFx, string searchTerm)
+    // {
+    //   List<Stylist> foundStylist = new List<Stylist> {};
+    //   if(searchFx.Equals("byStylist"))
+    //   {
+    //     foundStylist = Stylist.FindByStylistName(searchTerm);
+    //   }
+    //   return View("Index", foundStylist);
   }
 }
